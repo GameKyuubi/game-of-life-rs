67f15285c5e4a0d72b2dbf9a9eb2687dd2cac077
Michael Sodaro <xtra.zero@gmail.com>
2017-08-29 11:54:50 +0900

    Format tabs

--- src/app.rs
+++ src/app.rs
@@ -8,299 +8,299 @@ use rand::Rng;
 use std::cmp;
 
 pub struct AppSettings {
-    pub win_size: Size,
-    pub board_size: Size,
-    pub rect_size: i32,
+  pub win_size: Size,
+  pub board_size: Size,
+  pub rect_size: i32,
 }
 
 impl Default for AppSettings {
-    fn default() -> Self {
-        let win_size = Size(1024, 768);
-        let rect_size = 4;
-        let board_size = Size(win_size.0 / rect_size, win_size.1 / rect_size);
+  fn default() -> Self {
+    let win_size = Size(1024, 768);
+    let rect_size = 4;
+    let board_size = Size(win_size.0 / rect_size, win_size.1 / rect_size);
 
-        AppSettings {
-            win_size: win_size,
-            board_size: board_size,
-            rect_size: rect_size,
-        }
+    AppSettings {
+      win_size: win_size,
+      board_size: board_size,
+      rect_size: rect_size,
     }
+  }
 }
 
 pub struct App {
-    gl_graphics: GlGraphics,
+  gl_graphics: GlGraphics,
 
-    win_size: Size,
-    rect_size: i32,
-    offset: Move,
-    slide: Move,
+  win_size: Size,
+  rect_size: i32,
+  offset: Move,
+  slide: Move,
 
-    running: bool,
-    mouse_pos: Point,
-    onmouse_cell: Point,
+  running: bool,
+  mouse_pos: Point,
+  onmouse_cell: Point,
 
-    drawing: bool,
-    erasing: bool,
-    moving: Option<(Point, Move)>,
+  drawing: bool,
+  erasing: bool,
+  moving: Option<(Point, Move)>,
 
-    texture: Option<Texture>,
-    canvas: Option<ImageBuffer<Rgba<u8>, Vec<u8>>>,
-    invalidated: bool,
+  texture: Option<Texture>,
+  canvas: Option<ImageBuffer<Rgba<u8>, Vec<u8>>>,
+  invalidated: bool,
 
-    board: Board,
+  board: Board,
 }
 
 impl App {
-    pub fn new(settings: &AppSettings, gl_graphics: GlGraphics) -> Self {
-        App {
-            gl_graphics: gl_graphics,
+  pub fn new(settings: &AppSettings, gl_graphics: GlGraphics) -> Self {
+    App {
+      gl_graphics: gl_graphics,
 
-            win_size: settings.win_size,
-            rect_size: settings.rect_size,
-            offset: Move(0, 0),
-            slide: Move(0, 0),
+      win_size: settings.win_size,
+      rect_size: settings.rect_size,
+      offset: Move(0, 0),
+      slide: Move(0, 0),
 
-            running: true,
-            mouse_pos: Point(0, 0),
-            onmouse_cell: Point(0, 0),
+      running: true,
+      mouse_pos: Point(0, 0),
+      onmouse_cell: Point(0, 0),
 
-            drawing: false,
-            erasing: false,
-            moving: None,
+      drawing: false,
+      erasing: false,
+      moving: None,
 
-            texture: None,
-            canvas: None,
-            invalidated: true,
+      texture: None,
+      canvas: None,
+      invalidated: true,
 
-            board: Board::new_empty(settings.board_size),
-        }
+      board: Board::new_empty(settings.board_size),
     }
+  }
 
-    pub fn random_init<R>(&mut self, rng: &mut R)
-        where R: Rng
+  pub fn random_init<R>(&mut self, rng: &mut R)
+    where R: Rng
     {
-        self.board.random_init(rng);
-        self.invalidated = true;
+      self.board.random_init(rng);
+      self.invalidated = true;
     }
 
-    pub fn clear(&mut self) {
-        self.board.clear();
-        self.invalidated = true;
+  pub fn clear(&mut self) {
+    self.board.clear();
+    self.invalidated = true;
+  }
+
+  pub fn step(&mut self) {
+    if !self.running {
+      self.board.grow();
+      self.invalidated = true;
+    }
+  }
+
+  pub fn update(&mut self) {
+    if self.slide != Move(0, 0) {
+      self.offset = self.offset -
+        Move(self.slide.0 * self.rect_size, self.slide.1 * self.rect_size);
+      self.invalidated = true;
     }
 
-    pub fn step(&mut self) {
-        if !self.running {
-            self.board.grow();
-            self.invalidated = true;
+    if self.running {
+      self.board.grow();
+      self.invalidated = true;
+    }
+  }
+
+  pub fn set_win_size(&mut self, size: Size) {
+    if self.win_size != size {
+      self.win_size = size;
+      self.invalidated = true;
+      self.canvas = None;
+      self.texture = None;
+    }
+  }
+
+  pub fn fit_to_win_size(&mut self) {
+    let new_size = Size(self.win_size.0 / self.rect_size,
+                        self.win_size.1 / self.rect_size);
+    let mut board = Board::new_empty(new_size);
+
+    let x_off = if board.size().0 > self.board.size().0 {
+      (board.size().0 - self.board.size().0) / 2
+    } else {
+      0
+    };
+    let y_off = if board.size().1 > self.board.size().1 {
+      (board.size().1 - self.board.size().1) / 2
+    } else {
+      0
+    };
+
+    for x in 0..cmp::min(board.size().0, self.board.size().0) {
+      for y in 0..cmp::min(board.size().1, self.board.size().1) {
+        board.set(Point(x + x_off, y + y_off), self.board.get(Point(x, y)));
+      }
+    }
+
+    self.board = board;
+    self.invalidated = true;
+  }
+
+  pub fn toggle_running(&mut self) {
+    self.running = !self.running;
+  }
+
+  pub fn mouse_move(&mut self, mouse_pos: Point) {
+    self.mouse_pos = mouse_pos;
+
+    let onmouse_cell = self.pos2cell(mouse_pos);
+    let old_cell = self.onmouse_cell;
+    self.onmouse_cell = onmouse_cell;
+
+    if let Some((start_pos, start_offset)) = self.moving {
+      self.offset = start_offset + (mouse_pos - start_pos);
+    }
+
+    let op = match (self.drawing, self.erasing) {
+      (true, true) => None,
+      (true, false) => Some(true),
+      (false, true) => Some(false),
+      (false, false) => None,
+    };
+
+    if let Some(val) = op {
+      let diff = onmouse_cell - old_cell;
+      let a0 = diff.0.abs();
+      let a1 = diff.1.abs();
+      let a = cmp::max(a0, a1);
+      for i in 0..a {
+        let p = old_cell + Move(diff.0 * i / a, diff.1 * i / a);
+        if self.board.contains(p) {
+          self.board.set(p, val);
         }
+      }
+      if self.board.contains(onmouse_cell) {
+        self.board.set(onmouse_cell, val);
+      }
+      self.invalidated = true;
     }
+  }
 
-    pub fn update(&mut self) {
-        if self.slide != Move(0, 0) {
-            self.offset = self.offset -
-                          Move(self.slide.0 * self.rect_size, self.slide.1 * self.rect_size);
-            self.invalidated = true;
+  pub fn zoom(&mut self, scale: i32) {
+    let pos0 = ((self.mouse_pos.0 - self.offset.0) as f64) / (self.rect_size as f64);
+    let pos1 = ((self.mouse_pos.1 - self.offset.1) as f64) / (self.rect_size as f64);
+
+    if scale > 0 {
+      for _ in 0..scale.abs() {
+        if self.rect_size < 32 {
+          self.rect_size *= 2;
         }
-
-        if self.running {
-            self.board.grow();
-            self.invalidated = true;
+      }
+    } else {
+      for _ in 0..scale.abs() {
+        if self.rect_size > 1 {
+          self.rect_size /= 2;
         }
+      }
     }
 
-    pub fn set_win_size(&mut self, size: Size) {
-        if self.win_size != size {
-            self.win_size = size;
-            self.invalidated = true;
-            self.canvas = None;
-            self.texture = None;
+    self.offset = Move(((self.mouse_pos.0 as f64) - pos0 * (self.rect_size as f64)) as i32,
+    ((self.mouse_pos.1 as f64) - pos1 * (self.rect_size as f64)) as i32);
+    self.invalidated = true;
+  }
+
+  pub fn slide(&mut self, mv: Move) {
+    self.slide = self.slide + mv;
+  }
+
+  pub fn drawing(&mut self, val: bool) {
+    self.drawing = val;
+    let mouse_pos = self.mouse_pos;
+    self.mouse_move(mouse_pos);
+  }
+  pub fn erasing(&mut self, val: bool) {
+    self.erasing = val;
+    let mouse_pos = self.mouse_pos;
+    self.mouse_move(mouse_pos);
+  }
+  pub fn moving(&mut self, val: bool) {
+    if val {
+      self.moving = Some((self.mouse_pos, self.offset));
+    } else {
+      self.moving = None;
+    }
+  }
+
+  pub fn draw(&mut self, args: RenderArgs) {
+    self.adjust_offset();
+    self.update_texture();
+    let texture = self.texture.take().unwrap();
+
+    self.gl_graphics.draw(args.viewport(),
+    |ctx, g2d| graphics::image(&texture, ctx.transform, g2d));
+    self.texture = Some(texture);
+  }
+
+  fn board_size(&self) -> Size {
+    Size(self.rect_size * self.board.size().0,
+    self.rect_size * self.board.size().1)
+  }
+
+  fn adjust_offset(&mut self) {
+    let board_size = self.board_size();
+
+    self.offset.0 = if board_size.0 < self.win_size.0 {
+      (self.win_size.0 - board_size.0) / 2
+    } else {
+      clamp(self.offset.0, self.win_size.0 - board_size.0, 0)
+    };
+
+    self.offset.1 = if board_size.1 < self.win_size.1 {
+      (self.win_size.1 - board_size.1) / 2
+    } else {
+      clamp(self.offset.1, self.win_size.1 - board_size.1, 0)
+    };
+  }
+
+  fn update_texture(&mut self) {
+    if self.canvas.is_none() || self.texture.is_none() {
+      let canvas = ImageBuffer::new(self.win_size.0 as u32, self.win_size.1 as u32);
+      let texture = Texture::from_image(&canvas, &TextureSettings::new());
+      self.canvas = Some(canvas);
+      self.texture = Some(texture);
+      self.invalidated = true;
+    }
+
+    if self.invalidated {
+      self.invalidated = false;
+
+      let mut canvas = self.canvas.take().unwrap();
+      let mut texture = self.texture.take().unwrap();
+
+      for wx in 0..self.win_size.0 {
+        for wy in 0..self.win_size.1 {
+          let p = self.pos2cell(Point(wx, wy));
+          let color = if !self.board.contains(p) {
+            [128, 128, 128, 255]
+          } else if self.board.get(p) {
+            [255, 255, 255, 255]
+          } else {
+            [0, 0, 0, 255]
+          };
+          canvas.put_pixel(wx as u32, wy as u32, Rgba(color));
         }
+      }
+
+      texture.update(&canvas);
+
+      self.canvas = Some(canvas);
+      self.texture = Some(texture);
     }
+  }
 
-    pub fn fit_to_win_size(&mut self) {
-        let new_size = Size(self.win_size.0 / self.rect_size,
-                            self.win_size.1 / self.rect_size);
-        let mut board = Board::new_empty(new_size);
-
-        let x_off = if board.size().0 > self.board.size().0 {
-            (board.size().0 - self.board.size().0) / 2
-        } else {
-            0
-        };
-        let y_off = if board.size().1 > self.board.size().1 {
-            (board.size().1 - self.board.size().1) / 2
-        } else {
-            0
-        };
-
-        for x in 0..cmp::min(board.size().0, self.board.size().0) {
-            for y in 0..cmp::min(board.size().1, self.board.size().1) {
-                board.set(Point(x + x_off, y + y_off), self.board.get(Point(x, y)));
-            }
-        }
-
-        self.board = board;
-        self.invalidated = true;
-    }
-
-    pub fn toggle_running(&mut self) {
-        self.running = !self.running;
-    }
-
-    pub fn mouse_move(&mut self, mouse_pos: Point) {
-        self.mouse_pos = mouse_pos;
-
-        let onmouse_cell = self.pos2cell(mouse_pos);
-        let old_cell = self.onmouse_cell;
-        self.onmouse_cell = onmouse_cell;
-
-        if let Some((start_pos, start_offset)) = self.moving {
-            self.offset = start_offset + (mouse_pos - start_pos);
-        }
-
-        let op = match (self.drawing, self.erasing) {
-            (true, true) => None,
-            (true, false) => Some(true),
-            (false, true) => Some(false),
-            (false, false) => None,
-        };
-
-        if let Some(val) = op {
-            let diff = onmouse_cell - old_cell;
-            let a0 = diff.0.abs();
-            let a1 = diff.1.abs();
-            let a = cmp::max(a0, a1);
-            for i in 0..a {
-                let p = old_cell + Move(diff.0 * i / a, diff.1 * i / a);
-                if self.board.contains(p) {
-                    self.board.set(p, val);
-                }
-            }
-            if self.board.contains(onmouse_cell) {
-                self.board.set(onmouse_cell, val);
-            }
-            self.invalidated = true;
-        }
-    }
-
-    pub fn zoom(&mut self, scale: i32) {
-        let pos0 = ((self.mouse_pos.0 - self.offset.0) as f64) / (self.rect_size as f64);
-        let pos1 = ((self.mouse_pos.1 - self.offset.1) as f64) / (self.rect_size as f64);
-
-        if scale > 0 {
-            for _ in 0..scale.abs() {
-                if self.rect_size < 32 {
-                    self.rect_size *= 2;
-                }
-            }
-        } else {
-            for _ in 0..scale.abs() {
-                if self.rect_size > 1 {
-                    self.rect_size /= 2;
-                }
-            }
-        }
-
-        self.offset = Move(((self.mouse_pos.0 as f64) - pos0 * (self.rect_size as f64)) as i32,
-                           ((self.mouse_pos.1 as f64) - pos1 * (self.rect_size as f64)) as i32);
-        self.invalidated = true;
-    }
-
-    pub fn slide(&mut self, mv: Move) {
-        self.slide = self.slide + mv;
-    }
-
-    pub fn drawing(&mut self, val: bool) {
-        self.drawing = val;
-        let mouse_pos = self.mouse_pos;
-        self.mouse_move(mouse_pos);
-    }
-    pub fn erasing(&mut self, val: bool) {
-        self.erasing = val;
-        let mouse_pos = self.mouse_pos;
-        self.mouse_move(mouse_pos);
-    }
-    pub fn moving(&mut self, val: bool) {
-        if val {
-            self.moving = Some((self.mouse_pos, self.offset));
-        } else {
-            self.moving = None;
-        }
-    }
-
-    pub fn draw(&mut self, args: RenderArgs) {
-        self.adjust_offset();
-        self.update_texture();
-        let texture = self.texture.take().unwrap();
-
-        self.gl_graphics.draw(args.viewport(),
-                              |ctx, g2d| graphics::image(&texture, ctx.transform, g2d));
-        self.texture = Some(texture);
-    }
-
-    fn board_size(&self) -> Size {
-        Size(self.rect_size * self.board.size().0,
-             self.rect_size * self.board.size().1)
-    }
-
-    fn adjust_offset(&mut self) {
-        let board_size = self.board_size();
-
-        self.offset.0 = if board_size.0 < self.win_size.0 {
-            (self.win_size.0 - board_size.0) / 2
-        } else {
-            clamp(self.offset.0, self.win_size.0 - board_size.0, 0)
-        };
-
-        self.offset.1 = if board_size.1 < self.win_size.1 {
-            (self.win_size.1 - board_size.1) / 2
-        } else {
-            clamp(self.offset.1, self.win_size.1 - board_size.1, 0)
-        };
-    }
-
-    fn update_texture(&mut self) {
-        if self.canvas.is_none() || self.texture.is_none() {
-            let canvas = ImageBuffer::new(self.win_size.0 as u32, self.win_size.1 as u32);
-            let texture = Texture::from_image(&canvas, &TextureSettings::new());
-            self.canvas = Some(canvas);
-            self.texture = Some(texture);
-            self.invalidated = true;
-        }
-
-        if self.invalidated {
-            self.invalidated = false;
-
-            let mut canvas = self.canvas.take().unwrap();
-            let mut texture = self.texture.take().unwrap();
-
-            for wx in 0..self.win_size.0 {
-                for wy in 0..self.win_size.1 {
-                    let p = self.pos2cell(Point(wx, wy));
-                    let color = if !self.board.contains(p) {
-                        [128, 128, 128, 255]
-                    } else if self.board.get(p) {
-                        [255, 255, 255, 255]
-                    } else {
-                        [0, 0, 0, 255]
-                    };
-                    canvas.put_pixel(wx as u32, wy as u32, Rgba(color));
-                }
-            }
-
-            texture.update(&canvas);
-
-            self.canvas = Some(canvas);
-            self.texture = Some(texture);
-        }
-    }
-
-    fn pos2cell(&self, pos: Point) -> Point {
-        Point((pos.0 - self.offset.0) / self.rect_size,
-              (pos.1 - self.offset.1) / self.rect_size)
-    }
+  fn pos2cell(&self, pos: Point) -> Point {
+    Point((pos.0 - self.offset.0) / self.rect_size,
+    (pos.1 - self.offset.1) / self.rect_size)
+  }
 }
 
 fn clamp(val: i32, min: i32, max: i32) -> i32 {
-    cmp::min(cmp::max(val, min), max)
+  cmp::min(cmp::max(val, min), max)
 }
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ extern crate time;
 use app::{App, AppSettings};
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::input::{Button, Input, Key, MouseButton, MouseCursorEvent, MouseScrollEvent, PressEvent,
-                    ReleaseEvent, RenderEvent, ResizeEvent, UpdateEvent};
+ReleaseEvent, RenderEvent, ResizeEvent, UpdateEvent};
 use piston::window::{OpenGLWindow, WindowSettings};
 use sdl2_window::Sdl2Window;
 
@@ -18,182 +18,182 @@ mod app;
 mod board;
 
 fn main() {
-    let app_settings = AppSettings::default();
-    let opengl = OpenGL::V2_1;
-    let window: Sdl2Window = WindowSettings::new("Conway's Game of Life",
-                                                 (app_settings.win_size.0 as u32,
-                                                  app_settings.win_size.1 as u32))
-        .opengl(opengl)
-        .srgb(false)
-        .exit_on_esc(true)
-        .build()
-        .expect("failed to build Window");
-    let gl_graphics = GlGraphics::new(opengl);
+  let app_settings = AppSettings::default();
+  let opengl = OpenGL::V2_1;
+  let window: Sdl2Window = WindowSettings::new("Conway's Game of Life",
+                                               (app_settings.win_size.0 as u32,
+                                                app_settings.win_size.1 as u32))
+    .opengl(opengl)
+    .srgb(false)
+    .exit_on_esc(true)
+    .build()
+    .expect("failed to build Window");
+  let gl_graphics = GlGraphics::new(opengl);
 
-    let mut app = App::new(&app_settings, gl_graphics);
-    app.random_init(&mut rand::thread_rng());
+  let mut app = App::new(&app_settings, gl_graphics);
+  app.random_init(&mut rand::thread_rng());
 
-    event_loop::run(window, handle_event, app);
+  event_loop::run(window, handle_event, app);
 }
 
 fn handle_event(window: &mut Sdl2Window, e: Input, app: &mut App) {
-    if let Some(_args) = e.update_args() {
-        app.update();
-    }
+  if let Some(_args) = e.update_args() {
+    app.update();
+  }
 
-    if let Some(size) = e.resize_args() {
-        app.set_win_size(geom::Size(size[0] as i32, size[1] as i32));
-    }
+  if let Some(size) = e.resize_args() {
+    app.set_win_size(geom::Size(size[0] as i32, size[1] as i32));
+  }
 
-    if let Some(Button::Keyboard(key)) = e.press_args() {
-        match key {
-            Key::R => app.random_init(&mut rand::thread_rng()),
-            Key::C => app.clear(),
-            Key::S => app.toggle_running(),
-            Key::F => app.fit_to_win_size(),
-            Key::Space => app.step(),
-            Key::Equals => app.zoom(1),
-            Key::Minus => app.zoom(-1),
-            Key::Right => app.slide(geom::Move(1, 0)),
-            Key::Left => app.slide(geom::Move(-1, 0)),
-            Key::Up => app.slide(geom::Move(0, -1)),
-            Key::Down => app.slide(geom::Move(0, 1)),
-            _ => {}
-        }
+  if let Some(Button::Keyboard(key)) = e.press_args() {
+    match key {
+      Key::R => app.random_init(&mut rand::thread_rng()),
+      Key::C => app.clear(),
+      Key::S => app.toggle_running(),
+      Key::F => app.fit_to_win_size(),
+      Key::Space => app.step(),
+      Key::Equals => app.zoom(1),
+      Key::Minus => app.zoom(-1),
+      Key::Right => app.slide(geom::Move(1, 0)),
+      Key::Left => app.slide(geom::Move(-1, 0)),
+      Key::Up => app.slide(geom::Move(0, -1)),
+      Key::Down => app.slide(geom::Move(0, 1)),
+      _ => {}
     }
+  }
 
-    if let Some(Button::Keyboard(key)) = e.release_args() {
-        match key {
-            Key::Right => app.slide(geom::Move(-1, 0)),
-            Key::Left => app.slide(geom::Move(1, 0)),
-            Key::Up => app.slide(geom::Move(0, 1)),
-            Key::Down => app.slide(geom::Move(0, -1)),
-            _ => {}
-        }
+  if let Some(Button::Keyboard(key)) = e.release_args() {
+    match key {
+      Key::Right => app.slide(geom::Move(-1, 0)),
+      Key::Left => app.slide(geom::Move(1, 0)),
+      Key::Up => app.slide(geom::Move(0, 1)),
+      Key::Down => app.slide(geom::Move(0, -1)),
+      _ => {}
     }
+  }
 
-    if let Some(Button::Mouse(button)) = e.press_args() {
-        match button {
-            MouseButton::Left => app.drawing(true),
-            MouseButton::Right => app.erasing(true),
-            MouseButton::Middle => app.moving(true),
-            _ => {}
-        }
+  if let Some(Button::Mouse(button)) = e.press_args() {
+    match button {
+      MouseButton::Left => app.drawing(true),
+      MouseButton::Right => app.erasing(true),
+      MouseButton::Middle => app.moving(true),
+      _ => {}
     }
+  }
 
-    if let Some(Button::Mouse(button)) = e.release_args() {
-        match button {
-            MouseButton::Left => app.drawing(false),
-            MouseButton::Right => app.erasing(false),
-            MouseButton::Middle => app.moving(false),
-            _ => {}
-        }
+  if let Some(Button::Mouse(button)) = e.release_args() {
+    match button {
+      MouseButton::Left => app.drawing(false),
+      MouseButton::Right => app.erasing(false),
+      MouseButton::Middle => app.moving(false),
+      _ => {}
     }
+  }
 
-    if let Some(pos) = e.mouse_cursor_args() {
-        app.mouse_move(geom::Point(pos[0] as i32, pos[1] as i32));
-    }
+  if let Some(pos) = e.mouse_cursor_args() {
+    app.mouse_move(geom::Point(pos[0] as i32, pos[1] as i32));
+  }
 
-    if let Some(vol) = e.mouse_scroll_args() {
-        match vol[1] {
-            x if x > 0.0 => app.zoom(1),
-            x if x < 0.0 => app.zoom(-1),
-            _ => {}
-        }
+  if let Some(vol) = e.mouse_scroll_args() {
+    match vol[1] {
+      x if x > 0.0 => app.zoom(1),
+      x if x < 0.0 => app.zoom(-1),
+      _ => {}
     }
+  }
 
-    if let Some(args) = e.render_args() {
-        window.make_current();
-        app.draw(args);
-    }
+  if let Some(args) = e.render_args() {
+    window.make_current();
+    app.draw(args);
+  }
 }
 
 #[cfg(not(target_os = "emscripten"))]
 mod event_loop {
-    use piston::event_loop::{EventSettings, Events};
-    use piston::input::Input;
-    use sdl2_window::Sdl2Window;
+  use piston::event_loop::{EventSettings, Events};
+  use piston::input::Input;
+  use sdl2_window::Sdl2Window;
 
-    pub fn run<T>(mut window: Sdl2Window,
-                  handler: fn(window: &mut Sdl2Window, e: Input, arg: &mut T),
-                  mut arg: T) {
-        let mut events = Events::new(EventSettings::new());
-        while let Some(e) = events.next(&mut window) {
-            handler(&mut window, e, &mut arg);
-        }
+  pub fn run<T>(mut window: Sdl2Window,
+                handler: fn(window: &mut Sdl2Window, e: Input, arg: &mut T),
+                mut arg: T) {
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+      handler(&mut window, e, &mut arg);
     }
+  }
 }
 
 #[cfg(target_os = "emscripten")]
 mod event_loop {
-    extern crate emscripten_sys;
+  extern crate emscripten_sys;
 
-    use piston::input::{AfterRenderArgs, Input, RenderArgs, UpdateArgs};
-    use piston::window::Window;
-    use sdl2_window::Sdl2Window;
-    use std::mem;
-    use std::os::raw::c_void;
+  use piston::input::{AfterRenderArgs, Input, RenderArgs, UpdateArgs};
+  use piston::window::Window;
+  use sdl2_window::Sdl2Window;
+  use std::mem;
+  use std::os::raw::c_void;
 
-    struct EventLoop<T> {
-        last_updated: f64,
-        window: Sdl2Window,
-        handler: fn(window: &mut Sdl2Window, e: Input, arg: &mut T),
-        arg: T,
+  struct EventLoop<T> {
+    last_updated: f64,
+    window: Sdl2Window,
+    handler: fn(window: &mut Sdl2Window, e: Input, arg: &mut T),
+    arg: T,
+  }
+
+  pub fn run<T>(window: Sdl2Window,
+                handler: fn(window: &mut Sdl2Window, e: Input, arg: &mut T),
+                arg: T) {
+    unsafe {
+      let mut events = Box::new(EventLoop {
+        last_updated: emscripten_sys::emscripten_get_now() as f64,
+        window: window,
+        handler: handler,
+        arg: arg,
+      });
+      let events_ptr = &mut *events as *mut EventLoop<_> as *mut c_void;
+      emscripten_sys::emscripten_set_main_loop_arg(Some(main_loop_c::<T>), events_ptr, 0, 1);
+      mem::forget(events);
     }
+  }
 
-    pub fn run<T>(window: Sdl2Window,
-                  handler: fn(window: &mut Sdl2Window, e: Input, arg: &mut T),
-                  arg: T) {
-        unsafe {
-            let mut events = Box::new(EventLoop {
-                last_updated: emscripten_sys::emscripten_get_now() as f64,
-                window: window,
-                handler: handler,
-                arg: arg,
-            });
-            let events_ptr = &mut *events as *mut EventLoop<_> as *mut c_void;
-            emscripten_sys::emscripten_set_main_loop_arg(Some(main_loop_c::<T>), events_ptr, 0, 1);
-            mem::forget(events);
-        }
+  extern "C" fn main_loop_c<T>(arg: *mut c_void) {
+    unsafe {
+      let mut events: &mut EventLoop<T> = mem::transmute(arg);
+      let window = &mut events.window;
+      let handler = events.handler;
+      let arg = &mut events.arg;
+      window.swap_buffers();
+
+      let e = Input::AfterRender(AfterRenderArgs);
+      handler(window, e, arg);
+
+      while let Some(e) = window.poll_event() {
+        handler(window, e, arg);
+      }
+
+      if window.should_close() {
+        emscripten_sys::emscripten_cancel_main_loop();
+        return;
+      }
+
+      let now = emscripten_sys::emscripten_get_now() as f64;
+      let dt = now - events.last_updated;
+      events.last_updated = now;
+
+      let e = Input::Update(UpdateArgs { dt: dt });
+      handler(window, e, arg);
+
+      let size = window.size();
+      let draw_size = window.draw_size();
+      let e = Input::Render(RenderArgs {
+        ext_dt: dt,
+        width: size.width,
+        height: size.height,
+        draw_width: draw_size.width,
+        draw_height: draw_size.height,
+      });
+      handler(window, e, arg);
     }
-
-    extern "C" fn main_loop_c<T>(arg: *mut c_void) {
-        unsafe {
-            let mut events: &mut EventLoop<T> = mem::transmute(arg);
-            let window = &mut events.window;
-            let handler = events.handler;
-            let arg = &mut events.arg;
-            window.swap_buffers();
-
-            let e = Input::AfterRender(AfterRenderArgs);
-            handler(window, e, arg);
-
-            while let Some(e) = window.poll_event() {
-                handler(window, e, arg);
-            }
-
-            if window.should_close() {
-                emscripten_sys::emscripten_cancel_main_loop();
-                return;
-            }
-
-            let now = emscripten_sys::emscripten_get_now() as f64;
-            let dt = now - events.last_updated;
-            events.last_updated = now;
-
-            let e = Input::Update(UpdateArgs { dt: dt });
-            handler(window, e, arg);
-
-            let size = window.size();
-            let draw_size = window.draw_size();
-            let e = Input::Render(RenderArgs {
-                ext_dt: dt,
-                width: size.width,
-                height: size.height,
-                draw_width: draw_size.width,
-                draw_height: draw_size.height,
-            });
-            handler(window, e, arg);
-        }
-    }
+  }
 }
